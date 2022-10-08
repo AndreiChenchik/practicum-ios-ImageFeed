310c267d1a746b5a2f0a952c14a5c9d2d75cdf1d
Andrei Chenchik <andrei@chenchik.me>
2022-10-08 18:18:32 +0200

    Add viewModel and cell layout

---
+++ ImageFeed/ImageViewModel.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+struct ImageViewModel {
+    let image: UIImage
+    let date: Date
+    let isFavorite: Bool
+}
--- ImageFeed/ImagesListCell.swift
+++ ImageFeed/ImagesListCell.swift
@@ -1,5 +1,36 @@
 import UIKit
 
 class ImagesListCell: UITableViewCell {
-    
+    let mainImageView = UIImageView()
+    let dateLabel = UILabel()
+    let isFavoriteView = UIImageView()
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        mainImageView.layer.cornerRadius = 16
+        mainImageView.clipsToBounds = true
+
+        contentView.addSubview(mainImageView)
+        contentView.addSubview(dateLabel)
+        contentView.addSubview(isFavoriteView)
+
+        NSLayoutConstraint.activate([
+            mainImageView.topAnchor.constraint(
+                equalTo: contentView.topAnchor),
+            mainImageView.leadingAnchor.constraint(
+                equalTo: contentView.leadingAnchor),
+            mainImageView.trailingAnchor.constraint(
+                equalTo: contentView.trailingAnchor),
+            mainImageView.bottomAnchor.constraint(
+                equalTo: contentView.bottomAnchor)
+        ])
+    }
+
+    func configure(with viewModel: ImageViewModel) {
+        mainImageView.image = viewModel.image
+        dateLabel.text = viewModel.date.description
+        let icon = viewModel.isFavorite ? "heart.fill" : "heart"
+        isFavoriteView.image = viewModel.image
+    }
 }
