06b013ae8817594a6282b5a2770d31dfed1a6445
Andrei Chenchik <andrei@chenchik.me>
2022-10-15 18:02:14 +0200

    Update TableView

--- ImageFeed/ImagesListCell.swift
+++ ImageFeed/ImagesListCell.swift
@@ -1,69 +1,109 @@
 import UIKit
 
 class ImagesListCell: UITableViewCell {
-    let mainImageView: UIImageView = {
+    private let mainImageView: UIImageView
+    private let dateLabel: UILabel
+    private let isFavoriteView: UIImageView
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        mainImageView = Self.getImageView()
+        dateLabel = Self.getDateLabel()
+        isFavoriteView = UIImageView()
+
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        backgroundColor = .clear
+        setupComponents()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Cell configuration
+
+extension ImagesListCell {
+    func configure(with viewModel: ImageViewModel) {
+        mainImageView.image = viewModel.image
+        dateLabel.text = viewModel.dateString
+
+        let isFavorite = UIImage(named: "isFavorite")
+        let isNotFavorite = UIImage(named: "isNotFavorite")
+        isFavoriteView.image = viewModel.isFavorite ? isFavorite : isNotFavorite
+
+        layoutIfNeeded()
+    }
+}
+
+// MARK: - Cell components
+
+extension ImagesListCell {
+    private static func getImageView() -> UIImageView {
         let imageView = UIImageView()
 
         imageView.layer.cornerRadius = 16
         imageView.clipsToBounds = true
-        imageView.contentMode = .scaleAspectFill
+        imageView.contentMode = .scaleAspectFit
 
-        imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
-    }()
+    }
 
-    let dateLabel: UILabel = {
+    private static func getDateLabel() -> UILabel {
         let label = UILabel()
 
         let font = UIFont(name: "YSDisplay-Medium", size: 13)
         label.font = font
         label.textColor = .white
 
-        label.translatesAutoresizingMaskIntoConstraints = false
         return label
-    }()
+    }
 
-    let isFavoriteView: UIImageView = {
-        let imageView = UIImageView()
-
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        return imageView
-    }()
-
-    let gradientView: GradientView = {
+    private func getGradient() -> GradientView {
         let view = GradientView()
 
         let colors = [
             UIColor.clear.cgColor,
-            UIColor(colorAsset: .background).withAlphaComponent(0.2).cgColor
+            UIColor(colorAsset: .background).withAlphaComponent(0.9).cgColor
         ]
 
         let start = CGPointMake(0, 0)
         let end = CGPointMake(0, 0.5393)
 
         view.configure(colors: colors, start: start, end: end)
-
-        view.translatesAutoresizingMaskIntoConstraints = false
         return view
-    }()
-    
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+}
 
-        backgroundColor = .clear
+// MARK: - Cell layout
 
+extension ImagesListCell {
+    private func setupComponents() {
+        setupImageView()
+        setupGradient()
+        setupDataLabel()
+        setupFavoriteView()
+    }
+
+    private func setupImageView() {
+        mainImageView.translatesAutoresizingMaskIntoConstraints = false
         contentView.addSubview(mainImageView)
+
         NSLayoutConstraint.activate([
             mainImageView.topAnchor.constraint(
-                equalTo: contentView.topAnchor),
+                equalTo: contentView.topAnchor, constant: 4),
             mainImageView.leadingAnchor.constraint(
-                equalTo: contentView.leadingAnchor),
-            mainImageView.trailingAnchor.constraint(
-                equalTo: contentView.trailingAnchor),
+                equalTo: contentView.leadingAnchor, constant: 16),
+//            mainImageView.trailingAnchor.constraint(
+//                equalTo: contentView.trailingAnchor, constant: -16),
             mainImageView.bottomAnchor.constraint(
-                equalTo: contentView.bottomAnchor),
-            mainImageView.heightAnchor.constraint(equalToConstant: 200)
+                equalTo: contentView.bottomAnchor, constant: -4)
         ])
+    }
+
+    private func setupGradient() {
+        let gradientView = getGradient()
+        gradientView.translatesAutoresizingMaskIntoConstraints = false
 
         mainImageView.addSubview(gradientView)
         NSLayoutConstraint.activate([
@@ -75,16 +115,24 @@ class ImagesListCell: UITableViewCell {
                 equalTo: mainImageView.bottomAnchor),
             gradientView.heightAnchor.constraint(equalToConstant: 30)
         ])
+    }
 
+    private func setupDataLabel() {
+        dateLabel.translatesAutoresizingMaskIntoConstraints = false
         contentView.addSubview(dateLabel)
+
         NSLayoutConstraint.activate([
             dateLabel.leadingAnchor.constraint(
                 equalTo: mainImageView.leadingAnchor, constant: 8),
             dateLabel.bottomAnchor.constraint(
                 equalTo: mainImageView.bottomAnchor, constant: -8)
         ])
+    }
 
+    private func setupFavoriteView() {
+        isFavoriteView.translatesAutoresizingMaskIntoConstraints = false
         contentView.addSubview(isFavoriteView)
+
         NSLayoutConstraint.activate([
             isFavoriteView.trailingAnchor.constraint(
                 equalTo: mainImageView.trailingAnchor),
@@ -93,18 +141,5 @@ class ImagesListCell: UITableViewCell {
             isFavoriteView.heightAnchor.constraint(equalToConstant: 42),
             isFavoriteView.widthAnchor.constraint(equalToConstant: 42)
         ])
-    }
-
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    func configure(with viewModel: ImageViewModel) {
-        mainImageView.image = viewModel.image
-        dateLabel.text = viewModel.dateString
-
-        let isFavorite = UIImage(named: "isFavorite")
-        let isNotFavorite = UIImage(named: "isNotFavorite")
-        isFavoriteView.image = viewModel.isFavorite ? isFavorite : isNotFavorite
     }
 }
--- ImageFeed/ImagesListViewController.swift
+++ ImageFeed/ImagesListViewController.swift
@@ -1,41 +1,53 @@
 import UIKit
 
 class ImagesListViewController: UIViewController {
-
-    private let tableView = UITableView()
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setupView()
-        setupTable()
-        tableView.scrollToRow(at: .init(row: 0, section: 0), at: .top, animated: true)
-    }
-
     let mockData: [Picture] = {
         (0...20).map { num in
             Picture(
                 path: "\(num).png",
                 date: Date(),
-                isFavorite: .random()
+                isFavorite: num % 2 == 1
             )
         }
     }()
 
+    private let tableView = UITableView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureView()
+        configureTable()
+
+        updateInsets() // Imitate scroll position showed in design
+    }
+}
+
+// MARK: - Styling
+
+extension ImagesListViewController {
     override var preferredStatusBarStyle: UIStatusBarStyle { .lightContent }
 
-    private func setupView() {
-        view.backgroundColor = UIColor(colorAsset: .background)
+    private func updateInsets() {
+        tableView.contentInset =  UIEdgeInsets(
+            top: 16, left: 0, bottom: 0, right: 0
+        )
+        tableView.scrollToRow(
+            at: IndexPath(row: 0, section: 0), at: .top, animated: false
+        )
     }
 
-    private func setupTable() {
-        tableView.translatesAutoresizingMaskIntoConstraints = false
+    private func configureView() {
+        view.backgroundColor = UIColor(colorAsset: .background)
+    }
+}
 
+// MARK: - UITableView
+
+extension ImagesListViewController {
+    private func configureTable() {
         tableView.backgroundColor = .clear
         tableView.separatorStyle = .none
-        tableView.rowHeight = UITableView.automaticDimension
-        tableView.estimatedRowHeight = 100
-        tableView.contentInset = .init(top: 16, left: 0, bottom: 0, right: 0)
-        
+
         tableView.register(
             ImagesListCell.self,
             forCellReuseIdentifier: "\(ImagesListCell.self)")
@@ -43,26 +55,34 @@ class ImagesListViewController: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
 
+        tableView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(tableView)
 
         NSLayoutConstraint.activate([
             tableView.topAnchor.constraint(
                 equalTo: view.topAnchor),
             tableView.leadingAnchor.constraint(
-                equalTo: view.leadingAnchor, constant: 16),
+                equalTo: view.leadingAnchor),
             tableView.trailingAnchor.constraint(
-                equalTo: view.trailingAnchor, constant: -16),
+                equalTo: view.trailingAnchor),
             tableView.bottomAnchor.constraint(
                 equalTo: view.bottomAnchor)
         ])
+
+        tableView.estimatedRowHeight = 80
+        tableView.rowHeight = UITableView.automaticDimension
     }
 }
+
+// MARK: - UITableViewDelegate
 
 extension ImagesListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
     }
 }
+
+// MARK: - UITableViewDataSource
 
 extension ImagesListViewController: UITableViewDataSource {
     func tableView(
@@ -77,18 +97,15 @@ extension ImagesListViewController: UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(
             withIdentifier: "\(ImagesListCell.self)", for: indexPath)
 
-
         guard let imagesListCell = cell as? ImagesListCell else {
             fatalError("Can't get cell for ImagesList")
         }
 
-        configCell(for: imagesListCell)
         let viewModel = convert(model: mockData[indexPath.row])
         imagesListCell.configure(with: viewModel)
 
         return imagesListCell
     }
-
 
     private func convert(model: Picture) -> ImageViewModel {
         let image = UIImage(named: model.path) ?? .remove
@@ -101,6 +118,4 @@ extension ImagesListViewController: UITableViewDataSource {
             image: image, dateString: dateString, isFavorite: model.isFavorite
         )
     }
-
-    private func configCell(for cell: ImagesListCell) { }
 }
