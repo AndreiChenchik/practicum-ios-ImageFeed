1828bb21869bb0c38dd9631683d6dc5ddba23e6e
Andrei Chenchik <andrei@chenchik.me>
2022-10-22 23:29:11 +0200

    Add share icon

--- ImageFeed/Resources/ImageAsset.swift
+++ ImageFeed/Resources/ImageAsset.swift
@@ -2,6 +2,6 @@ import Foundation
 
 enum ImageAsset: String, CaseIterable {
     case isFavorite, isNotFavorite
-    case exitIcon, listTabIcon, profileTabIcon, backIcon
+    case exitIcon, listTabIcon, profileTabIcon, backIcon, shareIcon
     case mockUserPic
 }
