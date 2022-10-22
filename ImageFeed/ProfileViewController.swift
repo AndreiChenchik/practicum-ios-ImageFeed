987021b73542f6ee7bd99ccca5f1a40d813c1079
Andrei Chenchik <andrei@chenchik.me>
2022-10-22 16:26:06 +0200

    init ProfileViewController

---
+++ ImageFeed/ProfileViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Andrei Chenchik on 22/10/22.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
