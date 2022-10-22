acf69d5d5742901322006790f43e718ee13082bc
Andrei Chenchik <andrei@chenchik.me>
2022-10-22 21:27:31 +0200

    Fix tabBar appearance

--- ImageFeed/TabBarController.swift
+++ ImageFeed/TabBarController.swift
@@ -16,17 +16,24 @@ class TabBarController: UITabBarController {
 
         if let listItem = tabBar.items?[0] {
             listItem.image = .asset(.listTabIcon)
+            listItem.title = ""
         }
 
         if let profileItem = tabBar.items?[1] {
             profileItem.image = .asset(.profileTabIcon)
+            profileItem.title = ""
         }
     }
 
     private func setupAppearance() {
         let appearance = UITabBarAppearance()
+
         appearance.backgroundColor = .asset(.ypBlack)
         tabBar.standardAppearance = appearance
+
+        if #available(iOS 15.0, *) {
+            tabBar.scrollEdgeAppearance = appearance
+        }
 
         tabBar.tintColor = .asset(.ypWhite)
     }
@@ -37,6 +44,7 @@ class TabBarController: UITabBarController {
         )
 
         let appearance = UINavigationBarAppearance()
+
         appearance.backgroundColor = .asset(.ypBlack)
         controller.navigationBar.standardAppearance = appearance
 
