ecc49184e36b8ac0ed2e18c7312a6a3c096357f1
Andrei Chenchik <andrei@chenchik.me>
2022-10-22 23:12:14 +0200

    Add Zoom via ScrollView to SingleImageView

--- ImageFeed/Scenes/ImagesList/SingleImageView/SingleImageViewController.swift
+++ ImageFeed/Scenes/ImagesList/SingleImageView/SingleImageViewController.swift
@@ -8,18 +8,17 @@ final class SingleImageViewController: UITabBarController {
         super.viewDidLoad()
 
         setupView()
-        setupImageView()
+
+        setupScrollView()
         setupBackButton()
 
-        activateBackButton()
-
-        imageView.image = UIImage(named: "11")
+        layoutImageView()
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
-        imageView.image = image
+        if let image { displayImage(image) }
     }
 
     // MARK: Components
@@ -41,14 +40,39 @@ final class SingleImageViewController: UITabBarController {
 
         return button
     }()
+
+    private let scrollView: UIScrollView = {
+        let scroll = UIScrollView()
+
+        scroll.backgroundColor = .asset(.ypBlack)
+
+        return scroll
+    }()
 }
 
-// MARK: - Styling
-
 extension SingleImageViewController {
-    private func activateBackButton() {
+    private func setupBackButton() {
+        layoutBackButton()
         backButton.addTarget(
             self, action: #selector(backPressed), for: .touchUpInside)
+    }
+
+    private func setupScrollView() {
+        scrollView.delegate = self
+
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.25
+
+        layoutScrollView()
+    }
+}
+
+// MARK: - Actions
+
+extension SingleImageViewController {
+    private func displayImage(_ image: UIImage) {
+        imageView.image = image
+        rescaleAndCenterImageInScrollView(image: image)
     }
 
     @objc private func backPressed() {
@@ -59,24 +83,43 @@ extension SingleImageViewController {
 // MARK: - Layout
 
 extension SingleImageViewController {
-    private func setupImageView() {
-        imageView.translatesAutoresizingMaskIntoConstraints = false
+    private func layoutScrollView() {
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
 
-        view.addSubview(imageView)
+        view.addSubview(scrollView)
 
         NSLayoutConstraint.activate([
-            imageView.topAnchor.constraint(
+            scrollView.topAnchor.constraint(
                 equalTo: view.topAnchor),
-            imageView.leadingAnchor.constraint(
+            scrollView.leadingAnchor.constraint(
                 equalTo: view.leadingAnchor),
-            imageView.trailingAnchor.constraint(
+            scrollView.trailingAnchor.constraint(
                 equalTo: view.trailingAnchor),
-            imageView.bottomAnchor.constraint(
+            scrollView.bottomAnchor.constraint(
                 equalTo: view.bottomAnchor)
         ])
     }
 
-    private func setupBackButton() {
+    private func layoutImageView() {
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+
+        scrollView.addSubview(imageView)
+
+        let scrollGuide = scrollView.contentLayoutGuide
+
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(
+                equalTo: scrollGuide.topAnchor),
+            imageView.leadingAnchor.constraint(
+                equalTo: scrollGuide.leadingAnchor),
+            imageView.trailingAnchor.constraint(
+                equalTo: scrollGuide.trailingAnchor),
+            imageView.bottomAnchor.constraint(
+                equalTo: scrollGuide.bottomAnchor)
+        ])
+    }
+
+    private func layoutBackButton() {
         backButton.translatesAutoresizingMaskIntoConstraints = false
 
         view.addSubview(backButton)
@@ -97,5 +140,38 @@ extension SingleImageViewController {
 extension SingleImageViewController {
     private func setupView() {
         view.backgroundColor = .asset(.ypBlack)
+    }
+}
+
+// MARK: - UIScrollViewDelegate
+
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        imageView
+    }
+
+    private func rescaleAndCenterImageInScrollView(image: UIImage) {
+        let minZoomScale = scrollView.minimumZoomScale
+        let maxZoomScale = scrollView.maximumZoomScale
+
+        let containerSize = view.bounds.size
+        let imageSize = image.size
+
+        let hScale = containerSize.width / imageSize.width
+        let vScale = containerSize.height / imageSize.height
+
+        let scale = min(maxZoomScale,
+                        max(
+                            minZoomScale,
+                            max(hScale, vScale)))
+
+        scrollView.setZoomScale(scale, animated: false)
+        scrollView.layoutIfNeeded()
+
+        let newContentSize = scrollView.contentSize
+        let xVal = (newContentSize.width - containerSize.width) / 2
+        let yVal = (newContentSize.height - containerSize.height) / 2
+
+        scrollView.setContentOffset(CGPoint(x: xVal, y: yVal), animated: false)
     }
 }
