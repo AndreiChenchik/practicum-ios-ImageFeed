62a895aff0de4403c27b76b895c56cc00955817c
Andrei Chenchik <andrei@chenchik.me>
2022-10-22 16:41:11 +0200

    Move CustomColorAsset to separate file

--- ImageFeed/Helpers/UIColor+Extensions.swift
+++ ImageFeed/Helpers/UIColor+Extensions.swift
@@ -1,11 +1,6 @@
 import UIKit
 
 extension UIColor {
-    enum CustomColorAsset: String, CaseIterable {
-        case ypBlack
-        case white
-    }
-
     static func asset(_ colorAsset: CustomColorAsset) -> UIColor {
         UIColor(named: colorAsset.rawValue) ?? .clear
     }
---
+++ ImageFeed/Resources/CustomColorAsset.swift
@@ -0,0 +1,13 @@
+//
+//  CustomColorAsset.swift
+//  ImageFeed
+//
+//  Created by Andrei Chenchik on 22/10/22.
+//
+
+import Foundation
+
+enum CustomColorAsset: String, CaseIterable {
+    case ypBlack
+    case white
+}
