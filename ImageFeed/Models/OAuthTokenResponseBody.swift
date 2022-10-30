afcd459cbc65054deada3c5c29f29128d1955339
Andrei Chenchik <andrei@chenchik.me>
2022-10-30 18:43:46 +0100

    Add OAuthTokenResponseBody model

---
+++ ImageFeed/Models/OAuthTokenResponseBody.swift
@@ -0,0 +1,26 @@
+//
+//  OAuthTokenResponseBody.swift
+//  ImageFeed
+//
+//  Created by Andrei Chenchik on 30/10/22.
+//
+
+import Foundation
+
+struct OAuthTokenResponseBody: Codable {
+    let accessToken: String
+    let tokenType: TokenType
+    let scope: String
+    let createdTime: Date
+
+    enum TokenType: String, Codable {
+        case bearer
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case accessToken = "access_token"
+        case tokenType = "token_type"
+        case scope
+        case createdTime = "created_at"
+    }
+}
