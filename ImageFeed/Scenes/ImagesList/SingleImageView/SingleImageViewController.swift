d99f9072ac8a8788851fd12f0fb702e6269de036
Andrei Chenchik <andrei@chenchik.me>
2022-10-30 12:34:50 +0100

    Fix backButton paddings for SignleImageView

--- ImageFeed/Scenes/ImagesList/SingleImageView/SingleImageViewController.swift
+++ ImageFeed/Scenes/ImagesList/SingleImageView/SingleImageViewController.swift
@@ -152,9 +152,9 @@ extension SingleImageViewController {
 
         NSLayoutConstraint.activate([
             backButton.topAnchor.constraint(
-                equalTo: safeArea.topAnchor, constant: 9),
+                equalTo: safeArea.topAnchor, constant: 15),
             backButton.leadingAnchor.constraint(
-                equalTo: safeArea.leadingAnchor, constant: 9)
+                equalTo: safeArea.leadingAnchor, constant: 16)
         ])
     }
 
