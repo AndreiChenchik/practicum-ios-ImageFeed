fd01da871b78ed65625c5780498be4e5c7fca326
Andrei Chenchik <andrei@chenchik.me>
2022-12-08 13:19:32 +0100

    Update naming

--- ImageFeed/Models/PhotoResult.swift
+++ ImageFeed/Models/PhotoResult.swift
@@ -1,13 +1,13 @@
 import Foundation
 
-struct PhotoDTO: Decodable {
+struct PhotoResult: Decodable {
     let id: String
     let createdAt: Date
     let width: Int
     let height: Int
     let likedByUser: Bool
     let description: String?
-    let urls: PhotoURLs
+    let urls: PhotoUrlsResult
 
     enum CodingKeys: String, CodingKey {
         case id, width, height, description, urls
@@ -15,7 +15,7 @@ struct PhotoDTO: Decodable {
         case likedByUser = "liked_by_user"
     }
 
-    struct PhotoURLs: Decodable {
+    struct PhotoUrlsResult: Decodable {
         let regular: URL
         let thumb: URL
     }
--- ImageFeed/Services/ImagesListService.swift
+++ ImageFeed/Services/ImagesListService.swift
@@ -1,9 +1,13 @@
 final class ImagesListService {
     private (set) var photos: [Photo] = []
 
-    private var lastLoadedPage: Int?
+    private var imagesPerPage = 10
+
+    private var lastLoadedPage: Int {
+        photos.count / imagesPerPage
+    }
 
     func fetchPhotosNextPage() {
-        print("help")
+        print("kill me please")
     }
 }
